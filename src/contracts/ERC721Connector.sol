cb9ac536c9cc879ea5197a38489d08d766023b32
Real-Musafir <alshahadath@gmail.com>
2022-06-27 22:28:02 +0600

    inherit Kryptbird connector in kryptobird contract

---
+++ src/contracts/ERC721Connector.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import './ERC721Metadata.sol';
+
+contract ERC721Connector is ERC721Metadata {
+
+    // we deploy connector right away
+    // we want to metadata infor over
+
+    constructor(string memory name, string memory symbol) ERC721Metadata(name, symbol){
+        
+    }
+
+}
--- src/contracts/KryptoBirdz.sol
+++ src/contracts/KryptoBirdz.sol
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+import './ERC721Connector.sol';
 
-contract Kryptobird {
-    string public name;
-    string public symbol;
+contract Kryptobird is ERC721Connector {
 
-    constructor (){
-        name = 'Kryptobird';
-        symbol = "KBIRDZ";
-    }
+// initialize this contract to inherit
+// name and symbol from ERC721Metadata so that
+// the name is KryptoBird and the symbole is KBIRDZ
+
+constructor() ERC721Connector('Kryptobird', "KBIRDZ"){
+
+}
+
 }
