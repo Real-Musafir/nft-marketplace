94ff4aec62ea9b48e9137fbee33d916366d54f27
Real-Musafir <alshahadath@gmail.com>
2022-06-28 19:37:31 +0600

    mapping database for minting nft

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -5,14 +5,21 @@ pragma solidity >=0.4.22 <0.9.0;
 
 building out the minting function:
 a. nft to point to an address
-b. keep tract of the token ids
-c. keep tract of token owner address to token ids
-d. keep tract of how many tokens an owner address has
+b. keep track of the token ids
+c. keep track of token owner address to token ids
+d. keep track of how many tokens an owner address has
 e. create an event that emits a  transfer log - contract
  address, where  it is being minted to, the id
 
 */
 
-contract ERC721 { 
+contract ERC721 {
+    
+    // keep track of token owner address to token ids
+    mapping (uint256 => address) private _tokenOwner;
+
+    // keep track of how many tokens an owner address has
+    mapping (address => uint256) private _OwnedTokensCount;
+
 
 }
