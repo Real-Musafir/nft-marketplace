41989f3dc25b637682169997f2cb6925f0d5b45a
Real-Musafir <alshahadath@gmail.com>
2022-07-12 11:39:01 +0600

    create IERC721Metadata interface

--- src/contracts/ERC721Metadata.sol
+++ src/contracts/ERC721Metadata.sol
@@ -1,20 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract ERC721Metadata {
+import "./interfaces/IERC721Metadata.sol";
+
+contract ERC721Metadata is IERC721Metadata {
     string private _name;
     string private _symbol;
 
-    constructor(string memory named, string memory symbolified ){
+    constructor(string memory named, string memory symbolified) {
         _name = named;
         _symbol = symbolified;
     }
 
-    function name() external view returns(string memory){
+    function name() external view override returns (string memory) {
         return _name;
     }
 
-    function symbol() external view returns(string memory){
+    function symbol() external view override returns (string memory) {
         return _symbol;
     }
 }
