515fa10d85d4ed7dbf8695a99a3dacba14410b5c
Real-Musafir <alshahadath@gmail.com>
2022-07-13 11:34:14 +0600

    load web3 with provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,24 @@
 import React, { Component } from "react";
+import Web3 from "web3";
+import detectEthereumProvider from "@metamask/detect-provider";
+import KryptoBird from "../abis/Kryptobird.json";
 
 class App extends Component {
+  async componentDidMount() {
+    await this.loadWeb3();
+  }
+  // first up is to detect ethereum provider
+  async loadWeb3() {
+    const provider = await detectEthereumProvider();
+
+    if (provider) {
+      console.log("ethereum wallet is connected");
+      window.web3 = new Web3(provider);
+    } else {
+      console.log("No ethereum provider");
+    }
+  }
+
   render() {
     return (
       <div>
