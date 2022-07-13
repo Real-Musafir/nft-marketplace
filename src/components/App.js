61fa05341e8cc3d0de4a7452c85c20f442102aa8
Real-Musafir <alshahadath@gmail.com>
2022-07-13 17:48:01 +0600

    load contract to the frontend

--- src/components/App.js
+++ src/components/App.js
@@ -22,7 +22,19 @@ class App extends Component {
     const web3 = window.web3;
     const accounts = await web3.eth.getAccounts();
     this.setState({ account: accounts });
-    console.log(this.state.account, "okk");
+
+    // create a constant js valriable networkId Which
+    // is set to blockchain network id
+    const networkId = await web3.eth.net.getId();
+    const networkData = KryptoBird.networks[networkId];
+
+    if (networkData) {
+      const abi = KryptoBird.abi;
+      const address = networkData.address;
+      const contract = new web3.eth.Contract(abi, address);
+
+      console.log(contract, "okk");
+    }
   }
 
   constructor(props) {
@@ -44,9 +56,9 @@ class App extends Component {
             KryptoBird NFTs (Non Fungible Tokens)
           </div>
           <ul className="navbar-nav px-3">
-            <l className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
               <small className="text-white">{this.state.account}</small>
-            </l>
+            </li>
           </ul>
         </nav>
         <h1>NFT Marketplace</h1>
